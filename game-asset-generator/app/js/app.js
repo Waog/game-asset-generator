318396cbbe49b5416e6e9605f871b699d86fb5c1
oli <oliver.stadie@gmail.com>
2014-03-27 18:02:19 +0100

    Added second running pixi example (Loading from Sprite Sheets).

--- game-asset-generator/app/js/app.js
+++ game-asset-generator/app/js/app.js
@@ -1,17 +1,35 @@
 'use strict';
 
-
 // Declare app level module which depends on filters, and services
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.filters',
-  'myApp.services',
-  'myApp.directives',
-  'myApp.controllers'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/home', {templateUrl: 'partials/home.html', controller: 'nullCtrl'});
-  $routeProvider.when('/helpUs', {templateUrl: 'partials/helpUs.html', controller: 'nullCtrl'});
-  $routeProvider.when('/impressum', {templateUrl: 'partials/impressum.html', controller: 'nullCtrl'});
-  $routeProvider.otherwise({redirectTo: '/home'});
-}]);
+angular.module(
+		'myApp',
+		[ 'ngRoute', 'myApp.filters', 'myApp.services', 'myApp.directives',
+				'myApp.controllers' ]).config(
+		[ '$routeProvider', function($routeProvider) {
+			$routeProvider.when('/home', {
+				templateUrl : 'partials/home.html',
+				controller : 'nullCtrl'
+			});
+			$routeProvider.when('/helpUs', {
+				templateUrl : 'partials/helpUs.html',
+				controller : 'nullCtrl'
+			});
+			$routeProvider.when('/impressum', {
+				templateUrl : 'partials/impressum.html',
+				controller : 'nullCtrl'
+			});
+			$routeProvider.when('/pixiExperiments', {
+				redirectTo : '/pixiExample01'
+			});
+			$routeProvider.when('/pixiExample01', {
+				templateUrl : 'partials/pixiExperiments.html',
+				controller : 'pixi01Ctrl'
+			});
+			$routeProvider.when('/pixiExample02', {
+				templateUrl : 'partials/pixiExperiments.html',
+				controller : 'pixi02Ctrl'
+			});
+			$routeProvider.otherwise({
+				redirectTo : '/home'
+			});
+		} ]);
