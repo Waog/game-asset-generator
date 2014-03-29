84aeecf92c405896ee144c5bfb5c6ed8c5b0a3c4
oli <oliver.stadie@gmail.com>
2014-03-29 10:21:13 +0100

    First working experimental version of facebook login. Open TODOs: - Login Navigation bar need to initiate and repressent the login status. - Code Needs to be cleaned up.

--- game-asset-generator/app/js/controllers.js
+++ game-asset-generator/app/js/controllers.js
@@ -32,6 +32,22 @@ angular.module('myApp.controllers', [])
 						});
 						$scope.msg = "";
 					};
+
+					var auth = new FirebaseSimpleLogin(ref, function(error,
+							user) {
+						if (error) {
+							// an error occurred while attempting login
+							console.log(error);
+						} else if (user) {
+							// user authenticated with Firebase
+							console.log('User ID: ' + user.id
+									+ ', Provider: ' + user.provider);
+						} else {
+							// user is logged out
+						}
+					});
+
+					auth.login('facebook');
 				} ])
 
 .controller('navigationCtrl',
