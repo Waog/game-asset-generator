938a8016561d1998c9f540fd862012826c307253
oli <oliver.stadie@gmail.com>
2014-04-01 16:18:36 +0200

    Improved HTTP response from Google App Engine.

--- game-asset-generator/app/js/controllers.js
+++ game-asset-generator/app/js/controllers.js
@@ -3,271 +3,311 @@
 var controllerDefinitions = angular.module('myApp.controllers', []);
 
 controllerDefinitions.factory('gAnalyticsTrackService',
-        [ function gAnalyticsTrackService() {
-	        return function() {
-		        // ga('send', 'pageview');
-		        console.log('tracked something...');
-	        };
-        } ]);
+    [ function gAnalyticsTrackService() {
+	    return function() {
+		    // ga('send', 'pageview');
+		    console.log('tracked something...');
+	    };
+    } ]);
 
-controllerDefinitions.factory('authService', [
-        '$firebase',
+controllerDefinitions.factory('authService', [ '$firebase',
 
-        function authServiceFactory($firebase) {
-	        var ref = new Firebase(
-	                "https://game-asset-generator.firebaseio.com/");
+function authServiceFactory($firebase) {
+	var ref = new Firebase("https://game-asset-generator.firebaseio.com/");
 
-	        var anAuthServiceInstance = {
+	var anAuthServiceInstance = {
 
-	            firebaseRef : ref,
+	  firebaseRef : ref,
 
-	            loginWith : function(provider) {
-		            console.log('logging in with: ' + provider);
-		            // use the defined object to authenticate via facebook
-		            auth.login(provider);
-	            },
+	  loginWith : function(provider) {
+		  console.log('logging in with: ' + provider);
+		  // use the defined object to authenticate via facebook
+		  auth.login(provider);
+	  },
 
-	            isLoggedIn : function() {
-		            return this.user !== null;
-	            },
+	  isLoggedIn : function() {
+		  return this.user !== null;
+	  },
 
-	            getProvider : function() {
-		            return this.user.provider;
-	            },
+	  getProvider : function() {
+		  return this.user.provider;
+	  },
 
-	            getUserDisplayName : function() {
-		            return this.user.displayName;
-	            },
+	  getUserDisplayName : function() {
+		  return this.user.displayName;
+	  },
 
-	            user : null
-	        };
+	  user : null
+	};
 
-	        // define an object to handle authentication at firebase
-	        var auth = new FirebaseSimpleLogin(ref, function(error, user) {
-		        if (error) {
-			        // an error occurred while
-			        // attempting login
-			        console.log(error);
-		        } else if (user) {
-			        // user authenticated with Firebase
-			        console.log('User ID: ' + user.id + ', Provider: '
-			                + user.provider);
-			        anAuthServiceInstance.user = user;
-			        // $scope.$parent.naviLoginText = 'Welcome '
-			        // + user.displayName + ' logged in with: ';
-		        } else {
-			        // user is logged out
-		        }
-	        });
+	// define an object to handle authentication at firebase
+	var auth = new FirebaseSimpleLogin(ref, function(error, user) {
+		if (error) {
+			// an error occurred while
+			// attempting login
+			console.log(error);
+		} else if (user) {
+			// user authenticated with Firebase
+			console.log('User ID: ' + user.id + ', Provider: ' + user.provider);
+			anAuthServiceInstance.user = user;
+			// $scope.$parent.naviLoginText = 'Welcome '
+			// + user.displayName + ' logged in with: ';
+		} else {
+			// user is logged out
+		}
+	});
 
-	        return anAuthServiceInstance;
-        } ]);
+	return anAuthServiceInstance;
+} ]);
 
 controllerDefinitions.controller('nullCtrl', [ 'gAnalyticsTrackService',
-        function nullCtrl(gAnalyticsTrackService) {
-	        gAnalyticsTrackService();
-        } ]);
+    function nullCtrl(gAnalyticsTrackService) {
+	    gAnalyticsTrackService();
+    } ]);
 
 controllerDefinitions.controller('firebaseExperimentsCtrl', [
-        '$scope',
-        '$firebase',
-        'gAnalyticsTrackService',
-        'authService',
-        function firebaseExperimentsCtrl($scope, $firebase,
-                gAnalyticsTrackService, authService) {
-	        gAnalyticsTrackService();
+    '$scope',
+    '$firebase',
+    'gAnalyticsTrackService',
+    'authService',
+    function firebaseExperimentsCtrl($scope, $firebase, gAnalyticsTrackService,
+        authService) {
+	    gAnalyticsTrackService();
 
-	        $scope.messages = $firebase(authService.firebaseRef);
+	    $scope.messages = $firebase(authService.firebaseRef);
 
-	        $scope.addMessage = function(e) {
-		        if (e.keyCode != 13)
-			        return;
-		        $scope.messages.$add({
-		            name : $scope.newVarName,
-		            value : $scope.newVarValue
-		        });
-		        $scope.msg = "";
-	        };
-        } ]);
+	    $scope.addMessage = function(e) {
+		    if (e.keyCode != 13)
+			    return;
+		    $scope.messages.$add({
+		      name : $scope.newVarName,
+		      value : $scope.newVarValue
+		    });
+		    $scope.msg = "";
+	    };
+    } ]);
 
-controllerDefinitions.controller('gDriveExperimentsCtrl', [
-        'gAnalyticsTrackService', function gDriveExperimentsCtrl(gAnalyticsTrackService) {
-	        gAnalyticsTrackService();
-	        console.log('gDriveExperimentsCtrl entered');
-        } ]);
+controllerDefinitions
+    .controller(
+        'gDriveExperimentsCtrl',
+        [
+            'gAnalyticsTrackService',
+            '$http',
+            '$scope',
+            function gDriveExperimentsCtrl(gAnalyticsTrackService, $http,
+                $scope) {
+	            gAnalyticsTrackService();
+	            console.log('gDriveExperimentsCtrl entered');
+
+	            $scope.googleServerResponseData = 'Use the button to send a request to Google App Server .';
+
+	            $scope.sendHttpToGoogleAppEngine = function() {
+		            var googleHttpService = $http({
+		              method : 'GET',
+		              url : 'http://1-dot-expanded-system-536.appspot.com/game_asset_generator_server/uploadResource'
+		            });
+		            googleHttpService.success(function(data, status, headers,
+		                config) {
+			            // this callback will be called
+			            // asynchronously
+			            // when the response is
+			            // available
+			            console.log('Call to UploadResourceServlet with success: ');
+			            console.log('data:', data);
+			            console.log('status:', status);
+			            console.log('headers:', headers);
+			            console.log('config:', config);
+
+			            $scope.googleServerResponseData = data;
+		            });
+
+		            googleHttpService
+		                .error(function(data, status, headers, config) {
+			                // called asynchronously if an
+			                // error occurs
+			                // or server returns response
+			                // with an error status.
+			                console.log('Call to UploadResourceServlet with error: ');
+			                console.log('data:', data);
+			                console.log('status:', status);
+			                console.log('headers:', headers);
+			                console.log('config:', config);
+
+			                $scope.googleServerResponseData = 'Error, see console';
+		                });
+	            }
+
+            } ]);
 
 controllerDefinitions.controller('navigationCtrl', [
-        '$scope',
-        '$location',
-        'authService',
-        function navigationCtrl($scope, $location, authService) {
+    '$scope',
+    '$location',
+    'authService',
+    function navigationCtrl($scope, $location, authService) {
 
-	        /**
-			 * returns the 'active' css class if the given page matches the
-			 * current page.
+	    /**
+			 * returns the 'active' css class if the given page matches the current
+			 * page.
 			 */
-	        $scope.getCssNaviClass = function(page) {
-		        var currentRoute = $location.path().substring(1) || 'home';
-		        return page === currentRoute ? 'active' : '';
-	        };
+	    $scope.getCssNaviClass = function(page) {
+		    var currentRoute = $location.path().substring(1) || 'home';
+		    return page === currentRoute ? 'active' : '';
+	    };
 
-	        $scope.userAuth = {};
-	        $scope.naviLoginText = 'Login: ';
+	    $scope.userAuth = {};
+	    $scope.naviLoginText = 'Login: ';
 
-	        /**
-			 * returns the 'active' css class if the given (authentication)
-			 * provider matches the provider with is currently used for
-			 * authentication.
+	    /**
+			 * returns the 'active' css class if the given (authentication) provider
+			 * matches the provider with is currently used for authentication.
 			 */
-	        $scope.getCssLoginClass = function(provider) {
-		        if (authService.isLoggedIn()) {
-			        var curProvider = authService.getProvider();
-			        return provider === authService.getProvider() ? 'active'
-			                : '';
-		        } else {
-			        return '';
-		        }
-	        };
+	    $scope.getCssLoginClass = function(provider) {
+		    if (authService.isLoggedIn()) {
+			    var curProvider = authService.getProvider();
+			    return provider === authService.getProvider() ? 'active' : '';
+		    } else {
+			    return '';
+		    }
+	    };
 
-	        $scope.login = function(provider) {
-		        authService.loginWith(provider);
+	    $scope.login = function(provider) {
+		    authService.loginWith(provider);
 
-		        if (authService.isLoggedIn()) {
-			        $scope.naviLoginText = 'Welcome '
-			                + authService.getUserDisplayName()
-			                + '. Logged in with: ';
-		        }
-	        };
-        } ]);
+		    if (authService.isLoggedIn()) {
+			    $scope.naviLoginText = 'Welcome ' + authService.getUserDisplayName()
+			        + '. Logged in with: ';
+		    }
+	    };
+    } ]);
 
 controllerDefinitions.controller('pixi01Ctrl', [ 'gAnalyticsTrackService',
-        function pixi01Ctrl(gAnalyticsTrackService) {
-	        gAnalyticsTrackService();
+    function pixi01Ctrl(gAnalyticsTrackService) {
+	    gAnalyticsTrackService();
 
-	        // create an new instance of a pixi stage
-	        var stage = new PIXI.Stage(0x66FF99);
+	    // create an new instance of a pixi stage
+	    var stage = new PIXI.Stage(0x66FF99);
 
-	        // create a renderer instance
-	        // var renderer = new PIXI.WebGLRenderer(400, 300);
-	        var renderer = new PIXI.autoDetectRenderer(400, 300);
+	    // create a renderer instance
+	    // var renderer = new PIXI.WebGLRenderer(400, 300);
+	    var renderer = new PIXI.autoDetectRenderer(400, 300);
 
-	        // add the renderer view element to the DOM
-	        document.getElementById('pixiCanvas').appendChild(renderer.view);
+	    // add the renderer view element to the DOM
+	    document.getElementById('pixiCanvas').appendChild(renderer.view);
 
-	        requestAnimFrame(animate);
+	    requestAnimFrame(animate);
 
-	        // create a texture from an image path
-	        var texture = PIXI.Texture.fromImage("img/gears.png");
-	        // create a new Sprite using the texture
-	        var bunny = new PIXI.Sprite(texture);
+	    // create a texture from an image path
+	    var texture = PIXI.Texture.fromImage("img/gears.png");
+	    // create a new Sprite using the texture
+	    var bunny = new PIXI.Sprite(texture);
 
-	        // center the sprites anchor point
-	        bunny.anchor.x = 0.5;
-	        bunny.anchor.y = 0.5;
+	    // center the sprites anchor point
+	    bunny.anchor.x = 0.5;
+	    bunny.anchor.y = 0.5;
 
-	        // move the sprite t the center of the screen
-	        bunny.position.x = 200;
-	        bunny.position.y = 150;
+	    // move the sprite t the center of the screen
+	    bunny.position.x = 200;
+	    bunny.position.y = 150;
 
-	        stage.addChild(bunny);
+	    stage.addChild(bunny);
 
-	        function animate() {
+	    function animate() {
 
-		        requestAnimFrame(animate);
+		    requestAnimFrame(animate);
 
-		        // just for fun, lets rotate mr rabbit a little
-		        bunny.rotation += 0.1;
+		    // just for fun, lets rotate mr rabbit a little
+		    bunny.rotation += 0.1;
 
-		        // render the stage
-		        renderer.render(stage);
-	        }
-        } ]);
+		    // render the stage
+		    renderer.render(stage);
+	    }
+    } ]);
 
 controllerDefinitions.controller('pixi02Ctrl', [ 'gAnalyticsTrackService',
-        function pixi02Ctrl(gAnalyticsTrackService) {
-	        gAnalyticsTrackService();
-	        // create an array of assets to load
-	        var assetsToLoader = [ "img/pixiExperiments/ownSpriteSheet.json" ];
+    function pixi02Ctrl(gAnalyticsTrackService) {
+	    gAnalyticsTrackService();
+	    // create an array of assets to load
+	    var assetsToLoader = [ "img/pixiExperiments/ownSpriteSheet.json" ];
 
-	        // create a new loader
-	        var loader = new PIXI.AssetLoader(assetsToLoader);
+	    // create a new loader
+	    var loader = new PIXI.AssetLoader(assetsToLoader);
 
-	        // use callback
-	        loader.onComplete = onAssetsLoaded
+	    // use callback
+	    loader.onComplete = onAssetsLoaded
 
-	        // begin load
-	        loader.load();
+	    // begin load
+	    loader.load();
 
-	        // holder to store aliens
-	        var aliens = [];
-	        var alienFrames = [ "flag1.png", "Passage.png" ];
+	    // holder to store aliens
+	    var aliens = [];
+	    var alienFrames = [ "flag1.png", "Passage.png" ];
 
-	        var count = 0;
+	    var count = 0;
 
-	        // create an new instance of a pixi stage
-	        var stage = new PIXI.Stage(0xABCDEF);
-	        ;
+	    // create an new instance of a pixi stage
+	    var stage = new PIXI.Stage(0xABCDEF);
+	    ;
 
-	        // create a renderer instance.
-	        var renderer = PIXI.autoDetectRenderer(800, 600);
+	    // create a renderer instance.
+	    var renderer = PIXI.autoDetectRenderer(800, 600);
 
-	        // add the renderer view element to the DOM
-	        document.getElementById('pixiCanvas').appendChild(renderer.view);
+	    // add the renderer view element to the DOM
+	    document.getElementById('pixiCanvas').appendChild(renderer.view);
 
-	        // create an empty container
-	        var alienContainer = new PIXI.DisplayObjectContainer();
-	        alienContainer.position.x = 400;
-	        alienContainer.position.y = 300;
+	    // create an empty container
+	    var alienContainer = new PIXI.DisplayObjectContainer();
+	    alienContainer.position.x = 400;
+	    alienContainer.position.y = 300;
 
-	        stage.addChild(alienContainer);
+	    stage.addChild(alienContainer);
 
-	        function onAssetsLoaded() {
+	    function onAssetsLoaded() {
 
-		        // create a texture from an image path
-		        // add a bunch of aliens
-		        for (var i = 0; i < 10; i++) {
-			        var frameName = alienFrames[i % 2];
+		    // create a texture from an image path
+		    // add a bunch of aliens
+		    for (var i = 0; i < 10; i++) {
+			    var frameName = alienFrames[i % 2];
 
-			        // create an alien using the frame name..
-			        var alien = PIXI.Sprite.fromFrame(frameName);
+			    // create an alien using the frame name..
+			    var alien = PIXI.Sprite.fromFrame(frameName);
 
-			        /*
-					 * fun fact for the day :) another way of doing the above
-					 * would be var texture = PIXI.Texture.fromFrame(frameName);
-					 * var alien = new PIXI.Sprite(texture);
+			    /*
+					 * fun fact for the day :) another way of doing the above would be var
+					 * texture = PIXI.Texture.fromFrame(frameName); var alien = new
+					 * PIXI.Sprite(texture);
 					 */
 
-			        alien.position.x = Math.random() * 800 - 400;
-			        alien.position.y = Math.random() * 600 - 300;
-			        alien.anchor.x = 0.5;
-			        alien.anchor.y = 0.5;
-			        aliens.push(alien);
-			        alienContainer.addChild(alien);
+			    alien.position.x = Math.random() * 800 - 400;
+			    alien.position.y = Math.random() * 600 - 300;
+			    alien.anchor.x = 0.5;
+			    alien.anchor.y = 0.5;
+			    aliens.push(alien);
+			    alienContainer.addChild(alien);
 
-			        // TODO: remove git test
-		        }
+			    // TODO: remove git test
+		    }
 
-		        // start animating
-		        requestAnimFrame(animate);
+		    // start animating
+		    requestAnimFrame(animate);
 
-	        }
+	    }
 
-	        function animate() {
+	    function animate() {
 
-		        requestAnimFrame(animate);
+		    requestAnimFrame(animate);
 
-		        // just for fun, lets rotate mr rabbit a little
-		        for (var i = 0; i < 10; i++) {
-			        var alien = aliens[i];
-			        alien.rotation += 0.1;
-		        }
+		    // just for fun, lets rotate mr rabbit a little
+		    for (var i = 0; i < 10; i++) {
+			    var alien = aliens[i];
+			    alien.rotation += 0.1;
+		    }
 
-		        count += 0.01;
-		        alienContainer.scale.x = Math.sin(count);
-		        alienContainer.scale.y = Math.sin(count);
+		    count += 0.01;
+		    alienContainer.scale.x = Math.sin(count);
+		    alienContainer.scale.y = Math.sin(count);
 
-		        alienContainer.rotation += 0.01;
-		        // render the stage
-		        renderer.render(stage);
-	        }
-        } ]);
+		    alienContainer.rotation += 0.01;
+		    // render the stage
+		    renderer.render(stage);
+	    }
+    } ]);
