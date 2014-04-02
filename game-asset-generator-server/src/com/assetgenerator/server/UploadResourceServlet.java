c2638c4bd79a03a0132193b4d7abd18b1641407f
oli <oliver.stadie@gmail.com>
2014-04-02 15:40:50 +0200

    First working version of google file upload. - Added libraries for GDrive API - Added a static test-file to upload (it's somehow not used yet) - Added static Servlet-Code from the getting started with GDrive tutorial.
    It still has the following restrictions:
    - strange authorization process (needs to be adapted to our needs).
    - hard coded cliend ID and client secret (they are not commited to the
    repository but replaced by "TODO").
    - the files are uploaded to the users GDrive instead of our GDrive.
    - the tutorial says that the example-code is uploading a static
    test-file. In fact the application forces the user to select a file from
    his hard drive.

--- game-asset-generator-server/src/com/assetgenerator/server/UploadResourceServlet.java
+++ game-asset-generator-server/src/com/assetgenerator/server/UploadResourceServlet.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.Arrays;
 import java.util.Enumeration;
 
 import javax.servlet.ServletException;
@@ -15,6 +16,18 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.fileupload.FileItemIterator;
 import org.apache.commons.fileupload.FileItemStream;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
+
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.api.client.googleapis.auth.oauth2.GoogleTokenResponse;
+import com.google.api.client.http.FileContent;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.DriveScopes;
+import com.google.api.services.drive.model.File;
 
 public class UploadResourceServlet extends HttpServlet {
 
@@ -30,6 +43,7 @@ public class UploadResourceServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest req, HttpServletResponse res)
 			throws ServletException, IOException {
+		
 		// Set response content type
 		res.setContentType("text/html");
 
@@ -48,7 +62,7 @@ public class UploadResourceServlet extends HttpServlet {
 		out.println("req.getRemoteHost(): " + req.getRemoteHost());
 		out.println("req.getRemoteUser(): " + req.getRemoteUser());
 	}
-	
+
 	/**
 	 * @throws IOException
 	 * @throws ServletException
@@ -67,11 +81,13 @@ public class UploadResourceServlet extends HttpServlet {
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse res)
 			throws ServletException, IOException {
+		staticUploadToDrive();
+		
 		res.setHeader("Access-Control-Allow-Origin", "*");
 		res.setHeader("Access-Control-Allow-Methods",
 				"GET,POST,PUT,DELETE,OPTIONS");
 		res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-		
+
 		try {
 			StringBuilder sb = new StringBuilder("{\"result\": [");
 
@@ -164,5 +180,50 @@ public class UploadResourceServlet extends HttpServlet {
 		}
 		return sb.toString();
 
+	}
+
+	private static String CLIENT_ID = "TODO";
+	private static String CLIENT_SECRET = "TODO";
+
+//	private static String REDIRECT_URI = "urn:ietf:wg:oauth:2.0:oob";
+	private static String REDIRECT_URI = "http://localhost:8000/app/driveApiExample.html";
+
+	public static void staticUploadToDrive() throws IOException {
+		HttpTransport httpTransport = new NetHttpTransport();
+		JsonFactory jsonFactory = new JacksonFactory();
+
+		GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+				httpTransport, jsonFactory, CLIENT_ID, CLIENT_SECRET,
+				Arrays.asList(DriveScopes.DRIVE)).setAccessType("online")
+				.setApprovalPrompt("auto").build();
+
+		String url = flow.newAuthorizationUrl().setRedirectUri(REDIRECT_URI)
+				.build();
+		System.out
+				.println("Please open the following URL in your browser then type the authorization code:");
+		System.out.println("  " + url);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String code = br.readLine();
+
+		GoogleTokenResponse response = flow.newTokenRequest(code)
+				.setRedirectUri(REDIRECT_URI).execute();
+		GoogleCredential credential = new GoogleCredential()
+				.setFromTokenResponse(response);
+
+		// Create a new authorized API client
+		Drive service = new Drive.Builder(httpTransport, jsonFactory,
+				credential).build();
+
+		// Insert a file
+		File body = new File();
+		body.setTitle("My document");
+		body.setDescription("A test document");
+		body.setMimeType("text/plain");
+
+		java.io.File fileContent = new java.io.File("document.txt");
+		FileContent mediaContent = new FileContent("text/plain", fileContent);
+
+		File file = service.files().insert(body, mediaContent).execute();
+		System.out.println("File ID: " + file.getId());
 	}
 }
