cd3f1375f3af562770e0e4b1a36866543d9a1dd0
oli <oliver.stadie@gmail.com>
2014-04-02 14:43:38 +0200

    cleaned up. central place for server-url configurations now.

--- game-asset-generator/app/js/services.js
+++ game-asset-generator/app/js/services.js
@@ -2,8 +2,10 @@
 
 /* Services */
 
-
 // Demonstrate how to register services
 // In this case it is a simple value service.
-angular.module('myApp.services', []).
-  value('version', '0.1');
+var serviceDefinitions = angular.module('myApp.services', []);
+serviceDefinitions.value('version', '0.1');
+//serviceDefinitions.value('appEngineUrl', 'http://1-dot-expanded-system-536.appspot.com/');
+serviceDefinitions.value('appEngineUrl', 'http://localhost:8888/');
+serviceDefinitions.value('fileUploadRoute', 'game_asset_generator_server/uploadResource');
